428e5aa976fc04243462cb597310a2ee903cab7a
AbPru <appiaprudence2@gmail.com>
2023-04-21 22:45:03 -0700

    Task 8 updated

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,9 +8,9 @@ int main(void)
 {
 int num;
 char letter;
-for (num = 0; num < 10; num++)
+for (num = 0; num <= '9'; num++)
 {
-putchar((num % 10) + '0');
+putchar(num);
 for (letter = 'a'; letter <= 'f'; letter++)
 putchar(letter);
 }
